840578e454db6aaeee31c0500cd50aa9bed86f87
Muhammad Ahmad <hafizmuhammadahmad6211362@gmail.com>
2022-12-22 11:36:18 +0500

    Added faculty member Structure and changed the functions accordingly

--- README.md
+++ README.md
@@ -1,29 +1,15 @@
 # Inventory-Management-System
 
-*********************************************************************************************************************
-*																													*
-*																													*
-*									 	Final Term Project															*
-*																													*
-*									Inventory Management System														*
-*																													*
-*								 BCSF21M502 - Hafiz Muhammad Ahmad													*
-*																													*
-*																													*
-*********************************************************************************************************************
+* Final Term Project Programming Fundamentals
+* Topic: Inventory Management System		
+ * Submitted By: BCSF21M502 - Hafiz Muhammad Ahmad	
 
+> main.cpp      
+--->includes main function
 
-> main.cpp
-
-		-->includes main function
+> functions.h   
+      --->includes prototypes/declarations structures and importes included in main.cpp
 		
 		
-> functions.h
-
-		--> includes prototypes/declarations structures and importes
-		--> included in main.cpp
-		
-		
-> function.cpp
-
+> function.cp   
 		--> includes definations of functions
--- functions.h
+++ functions.h
@@ -14,14 +14,22 @@ const int ITEM_ID_LENGTH = 11;
 
 const string INVENTORY_DATA_FILE_ADDRESS = "inventory_item_data.txt";
 //const string INVENTORY_DATA_FILE_ADDRESS = "D:\\Ahmad\\Studies\\Semester 2\\Assignments_PF\\final_term_project\\inventory_item_data.txt";
+enum Department {
+    CS = 1, SE, IT, DS
+};
+
+struct FacMember {
+   char name[MAX_NAME_LENGTH + 1];
+   Department dept;
+};
 
 struct Inventory {
     char item_ID[ITEM_ID_LENGTH]{};
     char name[MAX_NAME_LENGTH + 1]{};  // 20 + (1 for null character).
     char category[MAX_NAME_LENGTH + 1]{};
     int item_count{};
-    int numberOfAllocations{};
-    char allocated_to[MAX_NAME_LENGTH + 1][MAX_ALLOCATIONS]{};
+    int numberOfAllocations=0;
+    FacMember allocated_to[MAX_ALLOCATIONS] = {};
     bool deleted = false;  // a flag for deleted inventory.
 };
 
@@ -44,6 +52,7 @@ void showAllPersonsAllocated();
 /***************************************** Input Tasks *******************************************************************/
 
 Inventory inputInventory();
+FacMember inputFacMember();
 
 /***************************************** Helping Functions *************************************************************/
 
@@ -54,16 +63,19 @@ void clearBuffer();
 void showAllInventoryNames();
 void displayAllocatedPersons( Inventory inv );
 
+string tellDept( Department dept );
+
 /***************************************** File Tasks *******************************************************************/
 
-bool writeInFile( const string & fileAddress, char * memoryAddress, size_t size, size_t startPoint = 0 );
-bool readFromFile( const string & fileAddress, char * memoryAddress, size_t size, size_t startPoint );
+bool writeInFile( const string & fileAddress, void * memoryAddress, size_t size, size_t startPoint = 0 );
+bool readFromFile( const string & fileAddress, void * memoryAddress, size_t size, size_t startPoint );
 bool fileInReadMode( fstream & file, const string & name );
 bool fileInWriteMode( fstream & file, const string & name );
 size_t numOfItemsInFile( const string & fileAddress, int sizeOfItem );
 size_t lastIndexOfFile( const string & fileAddress );
 void generateID( char id[] );
 unsigned int indexOfDeletedItemInFile( const string & fileAddress );
+unsigned int indexOfUndeletedItemInFile( unsigned int index, string fileAddress );
 
 /***************************************** Output Tasks *******************************************************************/
 
